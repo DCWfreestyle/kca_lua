b7fd623080b22858c0b75063ca1296e9d2ef6c08
Huang <dcwong09@gmail.com>
2016-02-11 12:49:52 +0800

    YZ mod

--- 4-5出击.lua
+++ 4-5出击.lua
@@ -3,6 +3,7 @@
 --require("\\Lua\\工具")
 
 --C:   477,213,7961087
+function c45()
 Kan.Sally(4,5)
 Kan.DelBattleInfo()
 Kan.AddBattleInfo(1,1,false,false)
@@ -11,6 +12,20 @@ Kan.AddBattleInfo(3,1,false,false)
 Kan.AddBattleInfo(4,1,true,false)
 Kan.AddWay(477,213)
 Kan.BattleEx(1,4,99,99,true)
+
+end
+
+function c25()
+Kan.Sally(2,5)
+Kan.DelBattleInfo()
+Kan.AddBattleInfo(1,1,false,false)
+Kan.AddBattleInfo(2,1,false,false)
+Kan.AddBattleInfo(3,1,true,false)
+Kan.BattleEx(1,3,99,99,false)
+
+end
+
+c25()
 
 补给.执行() 
 
--- 远征.lua
+++ 远征.lua
@@ -1,58 +1,7 @@
-
 require("\\Lua\\ztaskVer2")
---你只需要设置这三行数据即可
-k2_conquest = C.長距離練習航海_ID2
-k3_conquest = C.北方鼠輸送作戦_ID21
-k4_conquest = C.海上護衛任務_ID5
-
-check_time = {5,10}
---设置3-5分钟收回并放出一次远征
-
-------------------------------
-run_count = 0
-
---多任务相关
-men =0 ------开启调度
-number_of_tasks = 2 -------任务数量
-task_id = 0		------任务ID，从0开始
-
+require("\\Lua\\尝试远征")
 while true do
-	run_count = run_count + 1
-	Win.Print("开始第"..run_count.."次")
-
-	开始占用() ------###########################################
-	ret = Kan.ConquestGet()
-	--全部收回 会自动进入一次出击并返回母港来刷新出击状态
-	Base.Sleep(2000)
-	a,b,c,d = Kan.SupplyAll() --执行1234舰队的补给
-
-	Win.Print("补给完毕 尝试远征")
-
-	--判断队伍不在远征状态并且补给成功
-	if b ~= C.SUPPLY_STATE_CONQUEST and b ~= -1 and k2_conquest ~= nil then 
-		Kan.Conquest(2,k2_conquest[1],k2_conquest[2])
-	end
-
-	if c ~= C.SUPPLY_STATE_CONQUEST and c ~= -1 and k3_conquest ~= nil then
-		Kan.Conquest(3,k3_conquest[1],k3_conquest[2])
-	end
-
-	if d ~= C.SUPPLY_STATE_CONQUEST and d ~= -1 and k4_conquest ~= nil then
-		Kan.Conquest(4,k4_conquest[1],k4_conquest[2])
-	end
-
-	Win.Print("尝试远征完毕")
-
-	Kan.WaitHomeEx(2000,1000 * 120) --单次周期时间/超时时间
-
-	math.randomseed(os.time())
-	min = math.random(check_time[1],check_time[2]) --取随机分钟数3-5
-
-	Win.Print("等待"..min.."分钟")
-	
-	结束占用() ---###############################
-	Base.Sleep(1000*60*min) --等待
-
+	尝试一次远征()
+	Base.Sleep(1000*2)
 end
-
 Win.Pop('脚本执行完毕！')
