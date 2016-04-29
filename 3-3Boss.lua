b62f67cd9f98fc258c4e235594a2f8be16a459b0
Huang <dcwong09@gmail.com>
2016-04-29 23:38:16 +0800

    up2

--- 3-3Boss.lua
+++ 3-3Boss.lua
@@ -65,14 +65,25 @@ end
 
 
 function D出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f
---TODO:入参检验..
+
+    if ended then return end
     if cjn==nil then --initial
+        --入参检验..
+        if 出击策略.等待时间==nil then 出击策略.等待时间=0 end
+        if 出击策略.出击次数==nil then 出击策略.出击次数="inf" end --infinite
+        if type(出击策略.结束f)~="function" then 出击策略.结束f=0 end
+        if type(出击策略.换船f)~="function" then 出击策略.换船f=0 end
+        if type(出击策略.换船f)~="function" then 
+            ended=true
+            return
+        end
+        if type(出击策略.修理f)~="function" then 出击策略.修理f=0 end
         cjn = 1 --globa
         ended=false
         local ret=0
     end
-    if ended then return end
-    if (出击策略.出击次数>=1 and cjn>出击策略.出击次数) or (type(出击策略.结束f)=="function" and 出击策略.结束f()) then 
+    if (出击策略.出击次数>=1 and cjn>出击策略.出击次数) or ( 出击策略.结束f ~=0 and 出击策略.结束f()) then 
+        --结束判断以及结束后处理
         ended=true
         if rep==1 then
         全部修理()
@@ -96,17 +107,15 @@ function D出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f
             end
         end
     end
-
-    通用.等待母港(1000)
     if type(出击策略.换船f)=="function" then
         出击策略.换船f() --TODO
         ret = 补给.执行(true)
     end
     if ret==0 then
         if type(出击策略.换船f)=="function" then
-            出击策略.换船f("print_state")
+            --TODO：要求换船函数打印状态
+            --出击策略.换船f("print_state")
         end
-        通用.等待母港(2000)
         local k=出击策略.出击战斗f()
         if k==-10 then 
             刷新游戏()   --自动刷新
--- battletest.lua
+++ battletest.lua
@@ -1,26 +1,32 @@
-require("\\Lua\\changebynew")
+require("\\Lua\\Battle")
 require("\\Lua\\尝试远征")
-
 include_修船=true
 require("\\Lua\\明石修船")
-
-function 
-
-
+include_formedcag=true
+require("\\Lua\\formedcga")
 
 
+function D出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f,等待打印f
 
-end
-
-function 出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f
---TODO:入参检验..
-    if cjn=nil then --initial
+    if ended then return end
+    if cjn==nil then --initial
+        --入参检验..
+        if 出击策略.等待时间==nil then 出击策略.等待时间=0 end
+        if 出击策略.出击次数==nil then 出击策略.出击次数="inf" end --infinite
+        if type(出击策略.结束f)~="function" then 出击策略.结束f=0 end
+        if type(出击策略.换船f)~="function" then 出击策略.换船f=0 end
+        if type(出击策略.等待打印f)~="function" then 出击策略.等待打印f=0 end
+        if type(出击策略.换船f)~="function" then 
+            ended=true
+            return
+        end
+        if type(出击策略.修理f)~="function" then 出击策略.修理f=0 end
         cjn = 1 --globa
         ended=false
         local ret=0
     end
-    if ended then return end
-    if (出击策略.出击次数>=1 and cjn>出击策略.出击次数) or (type(出击策略.结束f)=="function" and 出击策略.结束f()) then 
+    if (出击策略.出击次数>=1 and cjn>出击策略.出击次数) or ( 出击策略.结束f ~=0 and 出击策略.结束f()) then 
+        --结束判断以及结束后处理
         ended=true
         if rep==1 then
         全部修理()
@@ -29,7 +35,11 @@ function 出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f
         return
     end
     if 出击策略.等待时间>0 and cjn>1 then
-        if os.time()<cj_last_time+出击策略.等待时间*60 then return end
+        if os.time()<cj_last_time+出击策略.等待时间*60 then 
+            if 出击策略.等待打印f~=0 then --等待时的操作
+                出击策略.等待打印f()
+            end
+        return end
     end
     Win.Print(("第:%d次"):format(cjn))
     ret = 补给.执行(true)
@@ -44,19 +54,15 @@ function 出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f
             end
         end
     end
-
-    通用.等待母港(1000)
-    
     if type(出击策略.换船f)=="function" then
         出击策略.换船f() --TODO
         ret = 补给.执行(true)
     end
-    
     if ret==0 then
         if type(出击策略.换船f)=="function" then
-            出击策略.换船f("print_state")
+            --TODO：要求换船函数打印状态
+            --出击策略.换船f("print_state")
         end
-        通用.等待母港(2000)
         local k=出击策略.出击战斗f()
         if k==-10 then 
             刷新游戏()   --自动刷新
@@ -67,10 +73,14 @@ function 出击(出击策略 ) --table 换船f ,出击战斗f,修理f,等待时间,出击次数,结束f
     通用.等待母港(2000)
 end
 
+
 function 出击and远征(出击策略)
     while true do 
-        出击(出击策略)
+        D出击(出击策略)
         尝试一次远征()
-        Base.Sleep(1000*2)
+        Base.Sleep(1000*5)
+		p("rest:",rest)
     end
 end
+--sample:
+--出击and远征({出击战斗f=function() 出击.执行(3, 3) return 战斗33() end,等待时间=6,结束f=function() return rest==0 end })
